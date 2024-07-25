a4aca97c78ab28da120129f5fc637fb17b0c500d
신진범 <shjb07015@gmail.com>
2024-07-25 17:03:47 +0900

    d

--- src/main/java/com/example/CRUD_Board_withSpringSecurity/Board/repository/PostRepository.java
+++ src/main/java/com/example/CRUD_Board_withSpringSecurity/Board/repository/PostRepository.java
@@ -1,7 +1,7 @@
 package com.example.CRUD_Board_withSpringSecurity.Board.repository;
-
 import com.example.CRUD_Board_withSpringSecurity.Board.domain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
 }
